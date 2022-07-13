b333061cdfa9818a72ee068fba9b8501d8ad2caa
Spasov <svetlio_spasov@abv.bg>
2022-07-13 14:52:03 +0300

    add

---
+++ Exceptions and Error Handling - Lab/Square Root/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Square_Root
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double n = double.Parse(Console.ReadLine());
+           
+
+
+           
+            try
+            {
+
+
+                CalculateSgrt( n);
+                
+
+               
+            }
+            catch (FormatException fe)
+            {
+
+                Console.WriteLine(fe.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+
+        private static void CalculateSgrt(double n)
+        {
+            if (n>=0)
+            {
+                Console.WriteLine(Math.Sqrt(n));
+
+            }
+            else
+            {
+               throw new FormatException("Invalid number.");
+            }
+        }
+    }
+}
