2b26731ee9939079c42f5933c9929776b46c5239
Spasov <svetlio_spasov@abv.bg>
2022-07-15 13:49:00 +0300

    add

---
+++ Exceptions and Error Handling - Lab/Money Transactions/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Money_Transactions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          string[] account = Console.ReadLine().Split(",");
+            Dictionary<int,double> acc = new Dictionary<int,double>();
+            for (int i = 0; i < account.Length; i++)
+            {
+                string[] arr = account[i].Split("-");
+                int bankNumber = int.Parse(arr[0]);
+                double sum = double.Parse(arr[1]);
+                acc.Add(bankNumber,sum);
+
+            }
+            string[] command = Console.ReadLine().Split();
+            while (command[0] != "End")
+            {
+                try
+                {
+                    if (command[0] == "Deposit")
+                    {
+                        int bankNumber = int.Parse(command[1]);
+                        double sum = double.Parse(command[2]);
+                        acc[bankNumber] += sum;
+                        Console.WriteLine($"Account {bankNumber} has new balance: {acc[bankNumber]}");
+                    }
+                    else if (command[0] == "Withdraw")
+                    {
+                        int bankNumber = int.Parse(command[1]);
+                        double sum = double.Parse(command[2]);
+                        if (acc[bankNumber]>= sum)
+                        {
+                        acc[bankNumber] -= sum;
+                            Console.WriteLine($"Account {bankNumber} has new balance: {acc[bankNumber]}");
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Insufficient balance!");
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid command!");
+                    }
+                }
+                catch (Exception)
+                {
+
+                    Console.WriteLine("Invalid account!");
+                }
+                finally
+                {
+                    Console.WriteLine("Enter another command");
+                }
+                command = Console.ReadLine().Split();
+            }
+        }
+    }
+}
