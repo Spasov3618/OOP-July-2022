ff24624cc0cee0767708d8c21b4f1660b74cd031
Spasov3618 <svetlio_spasov@abv.bg>
2022-08-20 08:03:33 +0300

    add

---
+++ testExamp/Repositories/UnitRepository.cs
@@ -0,0 +1,32 @@
+﻿using PlanetWars.Models.MilitaryUnits.Contracts;
+using PlanetWars.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlanetWars.Repositories
+{
+    public class UnitRepository : IRepository<IMilitaryUnit>
+    {
+        public UnitRepository()
+        {
+
+        }
+        public IReadOnlyCollection<IMilitaryUnit> Models => throw new NotImplementedException();
+
+        public void AddItem(IMilitaryUnit model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IMilitaryUnit FindByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveItem(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ testExamp/Repositories/WeaponRepository.cs
@@ -0,0 +1,33 @@
+﻿using PlanetWars.Models.Weapons.Contracts;
+using PlanetWars.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlanetWars.Repositories
+{
+    public class WeaponRepository : IRepository<IWeapon>
+    {
+
+        public WeaponRepository()
+        {
+
+        }
+        public IReadOnlyCollection<IWeapon> Models => throw new NotImplementedException();
+
+        public void AddItem(IWeapon model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IWeapon FindByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveItem(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
